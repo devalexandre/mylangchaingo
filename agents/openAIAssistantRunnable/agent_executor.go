fb98f85232f7de8da0555d5ced1c07a4d6b2dfb4
Alexandre E Souza <alexandre@dev2learn.com>
2024-06-30 19:40:23 -0300

    feat: add langsmith in AgentExecutor

--- agents/openAIAssistantRunnable/agent_executor.go
+++ agents/openAIAssistantRunnable/agent_executor.go
@@ -2,22 +2,44 @@ package openAIAssistantRunnable
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/devalexandre/langsmithgo"
+	"github.com/devalexandre/mylangchaingo"
+	"github.com/google/uuid"
 	"github.com/tmc/langchaingo/tools"
+	"log"
+	"os"
+	"runtime"
 )
 
 // AgentExecutor is responsible for executing the agent with the provided tools
 type AgentExecutor struct {
-	Agent *Assistant
-	Tools []tools.Tool
+	Agent           *Assistant
+	Tools           []tools.Tool
+	langsmithClient *langsmithgo.Client
 }
 
 // NewAgentExecutor creates a new instance of AgentExecutor
 func NewAgentExecutor(agent *Assistant, tools []tools.Tool) *AgentExecutor {
-	return &AgentExecutor{
+
+	agentExecutor := &AgentExecutor{
 		Agent: agent,
 		Tools: tools,
 	}
+	if os.Getenv("LANGCHAIN_TRACING") != "" && os.Getenv("LANGCHAIN_TRACING") != "false" {
+		client, err := langsmithgo.NewClient()
+		if err != nil {
+			log.Fatal(err)
+			return nil
+		}
+		agentExecutor.langsmithClient = client
+		root := uuid.New().String()
+		mylangchaingo.SetRunId(root)
+
+	}
+
+	return agentExecutor
 }
 
 // Invoke executes the agent with the provided input and returns the response
@@ -55,10 +77,47 @@ func (ae *AgentExecutor) HandleToolsExecution(threadID, runID string, toolCalls 
 			return fmt.Errorf("tool %s not found", toolCall.Function.Name)
 		}
 
+		if ae.langsmithClient != nil {
+			err := ae.langsmithClient.Run(&langsmithgo.RunPayload{
+				Name:        fmt.Sprintf("%v-%v-%v", langsmithgo.Tool, t.Name(), "AgentExecutor"),
+				SessionName: os.Getenv("LANGCHAIN_PROJECT_NAME"),
+				RunType:     langsmithgo.Tool,
+				RunID:       mylangchaingo.GetRunId(),
+				ParentID:    mylangchaingo.GetParentId(),
+				Inputs: map[string]interface{}{
+					"payload": extractArg1(toolCall.Function.Arguments),
+				},
+				Extras: map[string]interface{}{
+					"Metadata": map[string]interface{}{
+						"langsmithgo_version": "v1.0.0",
+						"go_version":          runtime.Version(),
+						"platform":            runtime.GOOS,
+						"arch":                runtime.GOARCH,
+					},
+				},
+			})
+
+			if err != nil {
+				return err
+			}
+		}
 		// Call the tool
 		toolOutput, err := t.Call(context.Background(), toolCall.Function.Arguments)
 		if err != nil {
 			return fmt.Errorf("failed to execute tool %s: %w", toolCall.Function.Name, err)
+		}
+
+		if ae.langsmithClient != nil {
+			err := ae.langsmithClient.Run(&langsmithgo.RunPayload{
+				RunID: mylangchaingo.GetRunId(),
+				Outputs: map[string]interface{}{
+					"output": extractArg1(toolOutput),
+				},
+			})
+
+			if err != nil {
+				return fmt.Errorf("error running langsmith: %w", err)
+			}
 		}
 
 		// Submit the tool output
@@ -69,4 +128,23 @@ func (ae *AgentExecutor) HandleToolsExecution(threadID, runID string, toolCalls 
 	}
 
 	return nil
+}
+
+func extractArg1(jsonStr string) (string, error) {
+	// Cria um mapa para armazenar os dados JSON.
+	var data map[string]string
+
+	// Desserializa a string JSON no mapa.
+	err := json.Unmarshal([]byte(jsonStr), &data)
+	if err != nil {
+		return "", err
+	}
+
+	// Retorna o valor de __arg1.
+	val, ok := data["__arg1"]
+	if !ok {
+		return "", fmt.Errorf("__arg1 key not found")
+	}
+
+	return val, nil
 }

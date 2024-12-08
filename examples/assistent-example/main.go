2dfbb03cf0f014ee91fd9cd2451063b8b6d4ed34
Alexandre E Souza <alexandre@dev2learn.com>
2024-12-07 22:29:15 -0300

    chore: resolve erros in examples

--- examples/assistent-example/main.go
+++ examples/assistent-example/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/devalexandre/mylangchaingo/agents/assistant"
+	"github.com/devalexandre/mylangchaingo/agents/assistant/executor"
 	"github.com/tmc/langchaingo/tools"
 )
 
@@ -10,19 +12,19 @@ func main() {
 
 	tool := tools.Calculator{}
 	assistant, err := assistant.NewAssistant(
-		"Calculator Assistant",
-		"You are a personal math tutor.",
-		"gpt-3.5-turbo",
-		[]tools.Tool{tool},
+		assistant.WithName("Calculator Assistant"),
+		assistant.WithDescription("You are a personal math tutor."),
+		assistant.WithModel("gpt-3.5-turbo"),
+		assistant.WithTools([]tools.Tool{tool}),
 	)
 	if err != nil {
 		fmt.Println("Error creating assistant:", err)
 		return
 	}
 
-	agentExecutor := assistant.NewAgentExecutor(assistant, []tools.Tool{tool})
+	agentExecutor := executor.NewAgentExecutor(assistant, executor.WithTools([]tools.Tool{tool}))
 
-	input := map[string]string{"content": "What is 10 + 20?"}
+	input := "What is 10 + 20?"
 	response, err := agentExecutor.Run(input)
 	if err != nil {
 		fmt.Println("Error invoking agent:", err)
